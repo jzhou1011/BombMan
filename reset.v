d194cb81031054aea4cccfa52a25f9335ec39c6f
Xiwei Ma <xiwei@ucla.edu>
2020-03-04 16:17:19 -0800

    top module arrays

--- reset.v
+++ reset.v
@@ -1,10 +1,12 @@
 module reset(
-	arena, bombs,
+	arena_0, arena_1, bombs_0, bombs_1, 
     rst, healthA, healthB, game_state
 );
 
-	output reg [1:0] arena [99:0];
-    output reg [1:0] bombs [99:0];
+	output reg arena_0 [99:0];
+    output reg bombs_0 [99:0];
+    output reg arena_1 [99:0];
+    output reg bombs_1 [99:0];
 
     input rst;
     output reg [1:0] healthA, healthB, game_state;
@@ -15,33 +17,53 @@ module reset(
         // initialize arena and bombs
         for (i = 0; i < 10; i = i+1) begin
 	    	for (j = 0; j < 10; j = j+1) begin
-                bombs[i*10+j] <= 0;
+                bombs_0[i*10+j] <= 0;
+                bombs_1[i*10+j] <= 0;
                 if (i == 0 || i == 9 || j == 0 || j == 9) begin
-                    arena[i*10+j] <= 1; // block
+                    arena_0[i*10+j] <= 1;
+                    arena_1[i*10+j] <= 0; // block
                 end
                 else begin
-                    arena[i*10+j] <= 0; // blank
+                    arena_0[i*10+j] <= 0;
+                    arena_1[i*10+j] <= 0;  // blank
                 end
 	    	end
         end
 
         // initialize players and blcks
-        arena[11] <= 2; // player A
-        arena[88] <= 3; // player B
-        arena[13] <= 1; // blocks
-        arena[17] <= 1;
-        arena[24] <= 1;
-        arena[32] <= 1;
-        arena[34] <= 1;
-        arena[38] <= 1;
-        arena[46] <= 1;
-        arena[51] <= 1;
-        arena[56] <= 1;
-        arena[57] <= 1;
-        arena[62] <= 1;
-        arena[63] <= 1;
-        arena[76] <= 1;
-        arena[84] <= 1;
+        arena_0[11] <= 0; // player A
+        arena_0[88] <= 1; // player B
+        arena_0[13] <= 1; // blocks
+        arena_0[17] <= 1;
+        arena_0[24] <= 1;
+        arena_0[32] <= 1;
+        arena_0[34] <= 1;
+        arena_0[38] <= 1;
+        arena_0[46] <= 1;
+        arena_0[51] <= 1;
+        arena_0[56] <= 1;
+        arena_0[57] <= 1;
+        arena_0[62] <= 1;
+        arena_0[63] <= 1;
+        arena_0[76] <= 1;
+        arena_0[84] <= 1;
+        
+        arena_1[11] <= 1; // player A
+        arena_1[88] <= 1; // player B
+        arena_1[13] <= 0; // blocks
+        arena_1[17] <= 0;
+        arena_1[24] <= 0;
+        arena_1[32] <= 0;
+        arena_1[34] <= 0;
+        arena_1[38] <= 0;
+        arena_1[46] <= 0;
+        arena_1[51] <= 0;
+        arena_1[56] <= 0;
+        arena_1[57] <= 0;
+        arena_1[62] <= 0;
+        arena_1[63] <= 0;
+        arena_1[76] <= 0;
+        arena_1[84] <= 0;
 
         healthA <= 3;
         healthB <= 3;
